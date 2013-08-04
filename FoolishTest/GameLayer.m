838e9e58d9a88282896b898256567a1ffe30fe6c
Bill <345310212@qq.com>
2013-08-04 22:43:23 +0800

    新的内容

--- FoolishTest/GameLayer.m
+++ FoolishTest/GameLayer.m
@@ -24,18 +24,35 @@
 -(id)init{
     if(self=[super init]){
         
-        [[CCSpriteFrameCache sharedSpriteFrameCache]addSpriteFramesWithFile:@"itemSprites.plist"];
-        [[CCSpriteFrameCache sharedSpriteFrameCache]addSpriteFramesWithFile:@"shapes.plist"];
+        [[CCSpriteFrameCache sharedSpriteFrameCache]addSpriteFramesWithFile:@"ItemSprites.plist"];
         
         winSize=[[CCDirector sharedDirector]winSize];
         
-        CCSprite *sprite=[CCSprite spriteWithSpriteFrameName:@"bluetriangle.png"];
-        [sprite setPosition:[self ccpHDWithX:480/2 y:320/2]];
-        [self addChild:sprite];
-        NSLog(@"x:%f,y:%f",sprite.position.x,sprite.position.y);
+        CCSprite *background=[CCSprite spriteWithFile:@"background.png"];
+        background.position=[self ccpHDWithX:480/2 y:320/2];
+        [self addChild:background];
+        
+        [self loadLevelWithIndex:0];
     }
     
     return self;
+}
+
+-(void)loadLevelWithIndex:(int)index{
+    NSString *path=[[NSBundle mainBundle]pathForResource:@"Levels" ofType:@"plist"];
+    NSDictionary *levelDictionary=[[[NSDictionary dictionaryWithContentsOfFile:path]objectForKey:@"Levels"]objectAtIndex:index];
+    
+    NSArray *levelObjects=[levelDictionary objectForKey:@"Objects"];
+    
+    for(NSDictionary *objectDictionary in levelObjects){
+        NSDictionary *objectPositionDictionary=[objectDictionary objectForKey:@"Position"];
+        
+        CGPoint objectPosition=[self ccpHDWithX:[[objectPositionDictionary objectForKey:@"x"]floatValue] y:[[objectPositionDictionary objectForKey:@"y"]floatValue]];
+        
+        CCSprite *sprite=[CCSprite spriteWithSpriteFrameName:[objectDictionary objectForKey:@"SpriteFrameName"]];
+        [sprite setPosition:ccp(objectPosition.x,objectPosition.y)];
+        [self addChild:sprite];
+    }
 }
 
 -(CGPoint)ccpHDWithX:(CGFloat)x y:(CGFloat)y{
